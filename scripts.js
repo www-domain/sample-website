b638dea9e6cf7513035ae8016207c654a99f7dae
Thomas <thomas@gmail.com>
2024-12-02 13:59:13 +0530

    Toggle Bg Color

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+let isLightBlue = false;   // Initial background color 
 function changeBg() {
-    document.body.style.backgroundColor = "lightblue"; 
+    // Check the current background color and toggle it 
+    if (isLightBlue) {
+        document.body.style.backgroundColor = ""; // Default color 
+    } else {
+        document.body.style.backgroundColor = "lightblue";
+    }
+    isLightBlue = !isLightBlue; // Toggle the boolean flag 
 }
